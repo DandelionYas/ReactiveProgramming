366ee2c63416a0e87803fcaee9e7966b6ee9cecc
Yaser Ghaderipour <yaser.ghaderipour@gmail.com>
2024-11-29 15:40:46 +0330

    Exercise 5: Custom subscriber and Backpressure

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,5 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
@@ -9,13 +12,45 @@ public class Exercise5 {
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(
+                System.out::println,
+                e -> System.out.println(e.getMessage()),
+                () -> System.out.println("Completed"));
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>());
 
         System.out.println("Press a key to end");
         System.in.read();
     }
+}
 
+class MySubscriber<T> extends BaseSubscriber<T> {
+    @Override
+    protected void hookOnSubscribe(Subscription subscription) {
+        System.out.println("Subscribe happened");
+        // Define the backpressure volume
+        // It says give me items when they are ready
+        // Otherwise the Publisher would not send it when its ready
+        request(1);
+    }
+
+    @Override
+    protected void hookOnNext(T value) {
+        System.out.println(value.toString() + " Received");
+        // It's still working based on push model
+        // Item will be sent based on producer's schedule
+        // It's just saying I'm ready for the next item, whenever you want to send
+        request(1);
+    }
+
+    @Override
+    protected void hookOnError(Throwable throwable) {
+        System.out.println(throwable.getMessage());
+    }
+
+    @Override
+    protected void hookOnComplete() {
+        System.out.println("Completed!");
+    }
 }
