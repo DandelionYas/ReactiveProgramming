4e5186f1e0ec7b6298d2a3c4f6a847c44700beb6
Yaser Ghaderipour <yaser.ghaderipour@gmail.com>
2024-12-02 09:55:22 +0330

    Exercise 6: Get value from unresponsive flux

--- src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise6.java
@@ -2,6 +2,7 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 
 public class Exercise6 {
 
@@ -11,11 +12,13 @@ public class Exercise6 {
         // Use ReactiveSources.unresponsiveFlux() and ReactiveSources.unresponsiveMono()
 
         // Get the value from the Mono into a String variable but give up after 5 seconds
-        ReactiveSources.unresponsiveMono().block(Duration.ofSeconds(5));
+        String foo = ReactiveSources.unresponsiveMono().block(Duration.ofSeconds(5));
 
         // Get the value from unresponsiveFlux into a String list but give up after 5 seconds
         // Come back and do this when you've learnt about operators!
-        // TODO: Write code here
+        List<String> list = ReactiveSources.unresponsiveFlux()
+                .collectList() // Gives us a Mono of the list: then we can block on mono to get the value
+                .block(Duration.ofSeconds(5));
 
         System.out.println("Press a key to end");
         System.in.read();
